6148ba198f8729ea8d94e822faa474ba00a15653
erdalHulagu <erdalhulahu@gmail.com>
2026-01-28 21:45:52 +0000

    28/JAN/26

--- src/main/java/com/erdal/helpdeskpro/repository/UserRepository.java
+++ src/main/java/com/erdal/helpdeskpro/repository/UserRepository.java
@@ -1,5 +1,32 @@
 package com.erdal.helpdeskpro.repository;
 
+import java.util.List;
+
+import com.erdal.helpdeskpro.domain.User;
+
 public class UserRepository {
+	
+	public void save(User user) {
+		
+		
+	}
+	
+	
+	public User findById(Long id) {
+		
+		return null;
+		
+	};
+	
+	public User findByEmail(String email) {
+		return null;
+	};
+	public boolean existsByEmail(String email) {
+		return true;
+	};
+	
+	public List<User> findAll() {
+		return null;
+	}
 
 }
