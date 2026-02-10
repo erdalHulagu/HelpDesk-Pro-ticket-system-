6d9aa080d5f072d99670dcd54bcef359d1cf0f9a
erdalHulagu <erdalhulahu@gmail.com>
2026-02-10 21:07:32 +0000

    10/FEB/26

--- src/main/java/com/erdal/helpdeskpro/service/impl/CommentServiceImpl.java
+++ src/main/java/com/erdal/helpdeskpro/service/impl/CommentServiceImpl.java
@@ -1,6 +1,5 @@
 package com.erdal.helpdeskpro.service.impl;
 
-
 import com.erdal.helpdeskpro.domain.Comment;
 import com.erdal.helpdeskpro.domain.Ticket;
 import com.erdal.helpdeskpro.domain.User;
@@ -20,52 +19,45 @@ public class CommentServiceImpl implements CommentService {
 	TicketRepository ticketRepository;
 	UserRepository userRepository;
 
-	public CommentServiceImpl(CommentRepository commentRepository,TicketRepository ticketRepository,UserRepository userRepository) {
+	public CommentServiceImpl(CommentRepository commentRepository, TicketRepository ticketRepository,
+			UserRepository userRepository) {
 		this.commentRepository = commentRepository;
 		this.ticketRepository = ticketRepository;
 		this.userRepository = userRepository;
 
 	}
-	
-	public void createComment(Comment comment,User user) {
-		
-		
-	
-		Ticket ticket=ticketRepository.findById(comment.getTicket().getId());
-		
-		
-		if (ticket==null) {
-			throw new ResourceNotFoundExeption(ExceptionMessage.TICKET_NOT_FOUND);
-			
-		}
-		if (ticket.isDeleted()==true) {
-			throw new ResourceNotFoundExeption(ExceptionMessage.TICKET_IS_DELETED);
-				
-		}
-		if (ticket.getStatus()==TicketStatus.CLOSED) {
-			throw new ResourceNotFoundExeption(ExceptionMessage.TICKET_IS_CLOSED);
-			
-		}
-		User autherUser=userRepository.findById(ticket.getCreatedBy().getId());
-		
-		
-		if (!(user.equals(autherUser)&&user.getRole()==Role.EMPLOYEE)||
-				user.getRole()==Role.EMPLOYEE) {
-			throw new BadRequestExeption(ExceptionMessage.NO_PERMITION);
-			
-		}
-		
-		 if (user.getRole() == Role.EMPLOYEE &&
-			        !ticket.getCreatedBy().getId().equals(user.getId())) {
-			        throw new BadRequestExeption(ExceptionMessage.NO_PERMITION);
-			    }
 
-			  
-			    comment.setAuthor(user);
-			    comment.setTicket(ticket);
-		        commentRepository.save(comment);
-		
-		
+	public void createComment(Comment comment, User user) {
+
+		Ticket ticket = ticketRepository.findById(comment.getTicket().getId());
+
+		if (ticket == null) {
+			throw new ResourceNotFoundExeption(ExceptionMessage.TICKET_NOT_FOUND);
+
+		}
+		if (ticket.isDeleted() == true) {
+			throw new ResourceNotFoundExeption(ExceptionMessage.TICKET_IS_DELETED);
+
+		}
+		if (ticket.getStatus() == TicketStatus.CLOSED) {
+			throw new ResourceNotFoundExeption(ExceptionMessage.TICKET_IS_CLOSED);
+
+		}
+		User autherUser = userRepository.findById(ticket.getCreatedBy().getId());
+
+		if (!(user.equals(autherUser) && user.getRole() == Role.EMPLOYEE) || user.getRole() == Role.EMPLOYEE) {
+			throw new BadRequestExeption(ExceptionMessage.NO_PERMITION);
+
+		}
+
+		if (user.getRole() == Role.EMPLOYEE && !ticket.getCreatedBy().getId().equals(user.getId())) {
+			throw new BadRequestExeption(ExceptionMessage.NO_PERMITION);
+		}
+
+		comment.setAuthor(user);
+		comment.setTicket(ticket);
+		commentRepository.save(comment);
+
 	}
 
 }
