89e4c8b262272bbed8a1725415af932beee4e294
erdalHulagu <erdalhulahu@gmail.com>
2026-02-13 23:58:50 +0000

    13/FEB/26

--- src/main/java/com/erdal/helpdeskpro/mapper/TicketMapper.java
+++ src/main/java/com/erdal/helpdeskpro/mapper/TicketMapper.java
@@ -1,7 +1,47 @@
 package com.erdal.helpdeskpro.mapper;
 
+import com.erdal.helpdeskpro.domain.Ticket;
+import com.erdal.helpdeskpro.domain.User;
+import com.erdal.helpdeskpro.dtos.TicketDTO;
+
 public class TicketMapper {
-	
-	
+
+	public static Ticket ticketDTOtoTicket(TicketDTO ticketDTO) {
+
+		User assignedUser = new User();
+		assignedUser.setId(ticketDTO.getAssignedToId());
+
+		User createdByUser = new User();
+		createdByUser.setId(ticketDTO.getCreatedById());
+
+		Ticket ticket = new Ticket();
+
+		ticket.setId(ticketDTO.getId());
+		ticket.setStatus(ticketDTO.getStatus());
+		ticket.setDescription(ticketDTO.getDescription());
+		ticket.setDeleted(ticketDTO.isDeleted());
+		ticket.setCreatedBy(createdByUser);
+		ticket.setAssignedTo(assignedUser);
+		ticket.setTitle(ticketDTO.getTitle());
+		
+		return ticket;
+
+	}
+	public static TicketDTO ticketToTicketDTO(Ticket ticket) {
+		
+		
+		TicketDTO ticketDTO = new TicketDTO();
+		
+		ticketDTO.setId(ticket.getId());
+		ticketDTO.setStatus(ticket.getStatus());
+		ticketDTO.setDescription(ticket.getDescription());
+		ticketDTO.setDeleted(ticket.isDeleted());
+		ticketDTO.setCreatedById(ticket.getCreatedBy().getId());
+		ticketDTO.setAssignedToId(ticket.getAssignedTo().getId());
+		ticketDTO.setTitle(ticket.getTitle());
+		
+		return ticketDTO;
+		
+	}
 
 }
